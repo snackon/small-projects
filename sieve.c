7d43e96840ac7aa313fb763d9e00307beec9883b
Snackon <Snacks@disroot.org>
2019-06-22 14:19:28 -0400

    fixed first two numbers not displaying properly

--- sieve.c
+++ sieve.c
@@ -7,17 +7,21 @@ int main(){
 	scanf("%d", &n);
 
 	bool arr[n];
+	for (int i = 0; i <= n; i++) {
+		arr[i] = i < 2 ? false : true;
+	}
+
 
 	for (int p = 2; p * p <= n; p++) {
-		if (arr[p] == false) {
+		if (arr[p] == true) {
 			for (int i = p * p; i <= n; i += p) {
-				arr[i] = true;
+				arr[i] = false;
 			}
 		}
 	}
 
 	for (int i = 0; i <= n; i++) {
-        printf("%d - %s\n", i, arr[i] == false ? "prime" : "non-prime");
+        printf("%d - %s\n", i, arr[i] == true ? "prime" : "non-prime");
     }
 	
 	return 0;
